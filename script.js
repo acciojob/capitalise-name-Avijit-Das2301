0bb4bd127565a1294174df5dc8d3fb0e19f00b17
Avijit_das2001 <55279276+Avijit-Das2301@users.noreply.github.com>
2025-12-26 23:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //your JS code here. If required.
-const nameInput=document.getElementById("fname");
-nameInput.addEvemtListener("blur",function () {
-	nameInput.value=nameInput.value.toUpperCase();	
+
+const nameInput = document.getElementById("fname");
+
+// Convert to uppercase as the user types
+nameInput.addEventListener("input", function () {
+  nameInput.value = nameInput.value.toUpperCase();
 });
