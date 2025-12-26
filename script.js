2bc6f05c35baf455aad0f9fe6dea89877114e0e1
Avijit_das2001 <55279276+Avijit-Das2301@users.noreply.github.com>
2025-12-26 23:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const nameInput=document.getElementById("fname");
+nameInput.addEvemtListener("blur",function () {
+	nameInput.value=nameInput.value.toUpperCase();	
+});
