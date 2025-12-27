69b94821edb1fe63831785cea4c96a114220bcff
Avijit_das2001 <55279276+Avijit-Das2301@users.noreply.github.com>
2025-12-28 00:43:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-//your JS code here. If required.
-
-// Get the input element with id="fname"
 const nameInput = document.getElementById("fname");
 
-// When the input field loses focus, convert its content to uppercase
 nameInput.addEventListener("blur", function () {
   nameInput.value = nameInput.value.toUpperCase();
 });
